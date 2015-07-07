92351bbea46dc3cd6160725aa4e659b84c845293
JollyDuck <jolly_duck@hotmail.co.uk>
2015-07-07 18:39:21 +0100

    Summer15: Added player Movement

---
+++ 15SummerHoliday/Assets/2d/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	//public SpriteRenderer sprRenderer;
+	//public Sprite[] sprArray = new Sprite[12];
+	public float ySpeed=100f;
+	public float xSpeed=100f;
+	private Rigidbody2D rb2;
+	// Use this for initialization
+	void Start () {
+	//	sprRenderer = GetComponent<SpriteRenderer>();
+		rb2 = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Input.GetAxis("Vertical")
+		if(Input.GetAxis("Vertical")!=0){
+			//Debug.Log ("W pressed!");
+			rb2.AddForce(Vector2.up*ySpeed*Input.GetAxis("Vertical"));
+		}
+
+
+		if(Input.GetAxis("Horizontal")!=0){
+			rb2.AddForce(Vector2.right*ySpeed*Input.GetAxis("Horizontal"));
+		}
+
+	}
+}
--- 15SummerHoliday/Assets/2d/levelGenerator.cs
+++ 15SummerHoliday/Assets/2d/levelGenerator.cs
@@ -1,18 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class makeRoom : MonoBehaviour {
-	public GameObject[] prefabs = new GameObject[7];
+public class levelGenerator : MonoBehaviour {
+	public GameObject[] prefabs = new GameObject[8];
 	public int widthOfTiles = 2;
+	public int mapSizeX = 50;
+	public int mapSizeY = 50;
 	//0-2 are floor
 	//3-4 are fancy floor
 	//5-6 are walls
+	//7 is !!!!!player!!!!!
+	//20 is blank
 	// Use this for initialization
 	void Start () {
-		genRoom(22,20);
+		int[,] array = new int[mapSizeX,mapSizeY];
+		for(int a = 0; a<mapSizeX; a++){
+			for(int b = 0; b<mapSizeY; b++){
+				array[a,b] = 20;
+				Debug.Log ("Assigned 20 to room");
+			}
+
+		}
+
+		//genRoom(22,20);
 	}
 
-	void genRoom(int width, int height){
+	/*
+	 * 1. Make an array of tiles filled with 'blank'
+	 * 2. Make a base room at 1,1 of size 10x10
+	 * 3. From an edge, take a 5-15 step walk in a random direction, then make another room
+	 * 4. Make X? amount of rooms
+	 * Rooms are defined by their bottom left corner and their width and height,
+	 * so 20,20,10,10 is a 10x10 room with bottom left starting at 20,20)
+	 * 
+	 * 
+	 * */
+
+	/*void genRoom(int width, int height){
 		int placeX = (int)transform.position.x - width/2;
 		int placeY = (int)transform.position.y - height/2;
 		for(int x=placeX; x<(placeX + width); x+=widthOfTiles){
@@ -33,7 +57,7 @@ public class makeRoom : MonoBehaviour {
 			}
 		}
 	}
-
+	*/
 	// Update is called once per frame
 	void Update () {
 	
